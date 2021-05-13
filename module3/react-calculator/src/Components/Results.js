48ae8a1609be7336d30c61e771fbd194e64e2263
Myra Smith <61550209+myrasmith@users.noreply.github.com>
2021-05-13 15:31:42 -0400

    Update Results.js

--- module3/react-calculator/src/Components/Results.js
+++ module3/react-calculator/src/Components/Results.js
@@ -5,6 +5,7 @@ export default class Results extends Component {
         //This will display the content from props of the calculations
         
         let {results} = this.props
+        // Here, you should try to figure out a way to format the numbers so that they have commas!
         return (
             <div className="results">
                 <p>{results}</p>
